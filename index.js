fd0690e5667b15e097433f4db5b9621783725bfe
AntonVoronezh <pstut@mail.ru>
2019-04-06 21:09:22 +0300

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const bodyParser = require('body-parser');
+const weatherRequest = require('./requests/weather.reques');
+
+const app = express();
+
+
+
+
