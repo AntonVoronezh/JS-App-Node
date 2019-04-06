aa8fa46a21dbb68c927e1ec5692520d8a0fbc80c
AntonVoronezh <pstut@mail.ru>
2019-04-06 21:05:56 +0300

    init

---
+++ requests/weather.reques.js
@@ -0,0 +1,13 @@
+const rp = require('request-promise');
+
+module.exports = async function(city = '') {
+    if(!city) {
+        throw new Error('Имя города не может быть пустым');
+    }
+
+
+
+
+
+    
+}
